73a5693c46e10db6cdb71e098d9989a02794d862
Higor <higorzen77@gmail.com>
2025-02-09 08:54:12 -0300

    1.0.0
    Inicio Desenolvimento Site do Conquer em ASP.NET usando MVC

---
+++ Controllers/RegistroController.cs
@@ -0,0 +1,50 @@
+﻿using ConquerSite.Data;
+using ConquerSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class RegistroController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public RegistroController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public ActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Register(PlayerModels model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = new PlayerModels
+            {
+                Username = model.Username,
+                Email = model.Email,
+                Password = model.Password,
+
+            };
+            _context.accounts.Add(user);
+            _context.SaveChanges();
+
+            return RedirectToAction("", "");
+        }
+
+        if (!ModelState.IsValid)
+        {
+            foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+            {
+                Console.WriteLine($"Erro: {error.ErrorMessage}");
+            }
+            return View(model);
+        }
+
+        Console.WriteLine("Modelo inválido");
+        return View(model);
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ConquerSite.Models;
+using Microsoft.EntityFrameworkCore;
+using ConquerSite.Models;
+
+
+namespace ConquerSite.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<PlayerModels> accounts { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PlayerModels>().ToTable("accounts"); // Nome exato da tabela no MySQL
+
+        }
+    }
+}
